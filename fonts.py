f1ec341ea3ce3e3636f1f8181ad9c8bd1fba8edc
Rob Ruana <rob@robruana.com>
2014-01-28 16:09:16 -0500

    Browser compatibility tweaks

--- fonts.py
+++ fonts.py
@@ -10,13 +10,16 @@ width = 0
 font = 0
 
 count = int((max_width - min_width) / width_step)
+max_font_str = str(min_font + (count * font_step)).rstrip('0').rstrip('.')
+max_width_str = str(max_width).rstrip('0').rstrip('.')
 
-print('''html{{font-size:{}px}}'''.format(str(min_font + (count * font_step)).rstrip('0').rstrip('.')))
+print('''html{font-size:16px}''')
+print('''@media all and (min-width:{0}px) and (min-height:{0}px){{html{{font-size:{1}px}}}}'''.format(max_width_str, max_font_str))
 
 template = '''@media all and (max-width:{0}px), all and (max-height:{0}px){{html{{font-size:{1}px}}}}'''
 
+print(template.format(max_width_str, max_font_str))
 for i in xrange(count - 1, -1, -1):
     width = str(min_width + (i * width_step)).rstrip('0').rstrip('.')
     font = str(min_font + (i * font_step)).rstrip('0').rstrip('.')
     print(template.format(width, font))
-
