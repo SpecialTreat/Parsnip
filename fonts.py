54bcf9435f630252839007f97563662eb36fce53
Rob Ruana <rob@robruana.com>
2014-01-28 07:26:51 -0500

    Website redesign

--- fonts.py
+++ fonts.py
@@ -1,10 +1,10 @@
 
-min_font = 6.75
-font_step = 0.25
+min_font = 7
+font_step = 0.5
 
 min_width = 240.0
 max_width = 2560.0
-width_step = 9.0
+width_step = 18.0
 
 width = 0
 font = 0
@@ -13,7 +13,7 @@ count = int((max_width - min_width) / width_step)
 
 print('''html{{font-size:{}px}}'''.format(str(min_font + (count * font_step)).rstrip('0').rstrip('.')))
 
-template = '''@media all and (max-width:{}px){{html{{font-size:{}px}}}}'''
+template = '''@media all and (max-width:{0}px), all and (max-height:{0}px){{html{{font-size:{1}px}}}}'''
 
 for i in xrange(count - 1, -1, -1):
     width = str(min_width + (i * width_step)).rstrip('0').rstrip('.')
