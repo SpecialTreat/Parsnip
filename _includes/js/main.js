5601b982b184cd6ed8a98269bc87a4335a488071
Rob Ruana <rob@robruana.com>
2015-02-07 23:47:37 -0500

    Test pure CSS3 parallax

---
+++ _includes/js/main.js
@@ -0,0 +1,25 @@
+
+
+// var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+// if (isMobile) {
+//     function addEvent(event, el, func) {
+//         if (el.addEventListener) {
+//             el.addEventListener(event, func, false);
+//         } else if (el.attachEvent) {
+//             el.attachEvent('on' + event, func);
+//         } else {
+//             el['on' + event] = func;
+//         }
+//     }
+
+//     addEvent('load', window, function() {
+//         window.scrollTo(0, 0);
+//     });
+// }
+
+// $(document).ready(function() {
+//     $('body').animate({backgroundColor: '#007aff'}, function() {
+//         $('#bubbles, #charity, #contact').animate({opacity: 1});
+//     });
+// });
