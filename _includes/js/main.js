c695e6caabe06084bc89a5aa9769c68e0a75aed9
Rob Ruana <rob@robruana.com>
2015-02-25 17:04:34 -0500

    First stab at parallax scrolling

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,25 +1,85 @@
 
 
-// var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+var agent = navigator.userAgent || navigator.vendor || window.opera || '';
+var regex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+var isMobile = regex.test(agent);
 
-// if (isMobile) {
-//     function addEvent(event, el, func) {
-//         if (el.addEventListener) {
-//             el.addEventListener(event, func, false);
-//         } else if (el.attachEvent) {
-//             el.attachEvent('on' + event, func);
-//         } else {
-//             el['on' + event] = func;
-//         }
-//     }
+// begin_appear
+// finish_appear
+// begin_disappear
+// finish_disappear
+var constants = {
 
-//     addEvent('load', window, function() {
-//         window.scrollTo(0, 0);
-//     });
-// }
+    home: 0,
+    home_begin_disappear: 900,
+    home_finish_disappear: 1400,
 
-// $(document).ready(function() {
-//     $('body').animate({backgroundColor: '#007aff'}, function() {
-//         $('#bubbles, #charity, #contact').animate({opacity: 1});
-//     });
-// });
+    title_begin_disappear: 0,
+    title_finish_disappear: 500,
+
+    title_bubbles_begin_appear: 0,
+    title_bubbles_finish_disappear: 800,
+
+    quote_bubbles_begin_appear: 750,
+    quote_bubbles_finish_disappear: 1750,
+
+    quotes_begin_appear: 200,
+    quotes_finish_appear: 600,
+    quotes_begin_disappear: 900,
+    quotes_finish_disappear: 1400,
+
+    quote_a_begin_appear: 200,
+    quote_a_finish_appear: 700,
+
+    quote_b_begin_appear: 400,
+    quote_b_finish_appear: 900,
+
+    design: 1400,
+    parsnip: 1900,
+    parsnip_slides_begin_appear: 1550,
+    parsnip_slides_finish_appear: 1800,
+    parsnip_begin_disappear: 1900,
+    parsnip_finish_disappear: 2400,
+    charity: 2400,
+    contact_begin_appear: 2650,
+    charity_finish_disappear: 2900,
+    contact: 3025,
+    end: 3250
+};
+
+var s = skrollr.init({
+    edgeStrategy: 'set',
+    smoothScrolling: true,
+    mobileCheck: function() { return isMobile; },
+    easing: {
+        inOut: function(p) {
+            if (p <= 0.5) {
+                return 2 * p * p;
+            } else {
+                return 1 - (2 * (1 - p) * (1 - p));
+            }
+        }
+    },
+    constants: constants
+});
+
+skrollr.menu.init(s, {
+    handleLink: function(link) {
+        var hash = link.getAttribute('href');
+        var id = hash.substr(1);
+        return constants[id];
+    }
+});
+
+var ids = ['quotes', 'design', 'parsnip', 'charity', 'contact'];
+for(var i = 1; i <= 10; i++) {
+    ids.push('bubble-layer-' + i);
+}
+
+var ids_len = ids.length;
+for(var i = 0; i < ids_len; i++) {
+    var id = ids[i];
+    var el = document.getElementById(id);
+    el.style.display = 'inherit';
+}
+
