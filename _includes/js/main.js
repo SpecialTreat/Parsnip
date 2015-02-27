db53fd8bc1bcc6ca8625abf144658c6c98d9ad60
Rob Ruana <rob@robruana.com>
2015-02-27 15:29:52 -0500

    Revert to one page Parsnip website. No more parallax

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,90 +1,32 @@
-
-
 var agent = navigator.userAgent || navigator.vendor || window.opera || '';
 var regex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
 var isMobile = regex.test(agent);
-
-// begin_appear
-// finish_appear
-// begin_disappear
-// finish_disappear
-var constants = {
-
-    home: 0,
-    home_begin_disappear: 900,
-    home_finish_disappear: 1400,
-
-    title_begin_disappear: 0,
-    title_finish_disappear: 500,
-
-    title_bubbles_begin_appear: 0,
-    title_bubbles_finish_disappear: 1000,
-
-    quote_bubbles_begin_appear: 700,
-    quote_bubbles_finish_disappear: 2100,
-
-    quotes_begin_appear: 200,
-    quotes_finish_appear: 600,
-    quotes_begin_disappear: 900,
-    quotes_finish_disappear: 1400,
-
-    quote_a_begin_appear: 200,
-    quote_a_finish_appear: 700,
-
-    quote_b_begin_appear: 400,
-    quote_b_finish_appear: 800,
-
-    design: 1400,
-
-    parsnip: 1900,
-    parsnip_begin_disappear: 1950,
-    parsnip_finish_disappear: 2550,
-
-    charity: 2550,
-    charity_finish_disappear: 3150,
-
-    contact_begin_appear: 2900,
-    contact: 3300,
-
-    end: 3350
-};
-
-var s = skrollr.init({
-    edgeStrategy: 'set',
-    smoothScrolling: true,
-    mobileDeceleration: 0.004,
-    mobileCheck: function() { return isMobile; },
-    easing: {
-        inOut: function(p) {
-            if (p <= 0.5) {
-                return 2 * p * p;
-            } else {
-                return 1 - (2 * (1 - p) * (1 - p));
-            }
-        }
-    },
-    constants: constants
-});
-
-skrollr.menu.init(s, {
-    handleLink: function(link) {
-        var hash = link.getAttribute('href');
-        var id = hash.substr(1);
-        return constants[id];
-    }
-});
-
-var ids = ['quotes', 'design', 'parsnip', 'charity', 'contact'];
-for(var i = 1; i <= 8; i++) {
-    ids.push('bubble-layer-' + i);
+if (isMobile) {
+    var getWindowHeight = function() {
+        // Get zoom level of mobile Safari
+        // Note, that such zoom detection might not work correctly in other
+        // browsers. We use width, instead of height, because there are no
+        // vertical toolbars :)
+        var zoomLevel = (document.documentElement.clientWidth /
+                         window.innerWidth);
+        // window.innerHeight returns height of the visible area.
+        // We multiply it by zoom and get out real height.
+        return window.innerHeight * zoomLevel;
+    };
+} else {
+    var getWindowHeight = function() {
+        return $(window).height()
+    };
 }
-
-var ids_len = ids.length;
-for(var i = 0; i < ids_len; i++) {
-    var id = ids[i];
-    var el = document.getElementById(id);
-    if(el) {
-        el.style.display = 'inherit';
-    }
-}
+jQuery(document).ready(function($) {
+    var $document = $(document),
+        $htmlbody = $('html,body'),
+        $contactLink = $('#contact-link');
+    $contactLink.click(function(event) {
+        event.preventDefault();
+        $htmlbody.animate({
+            scrollTop: $document.height() - getWindowHeight()
+        }, 500, 'easeInOutQuint');
+    });
+});
 
